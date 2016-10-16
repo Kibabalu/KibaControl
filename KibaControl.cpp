bd2d9721ca41ea854cf7a61d42d0ca23a863fdf7
Kibabalu <kiba.balu@me.com>
2016-10-16 17:59:00 +0200

    Please enter the commit message for your changes. Lines starting

--- KibaControl.cpp
+++ KibaControl.cpp
@@ -1,20 +1,20 @@
 /*------------------------------------------------------------------------------------------------*/
-/*	KibaControl		Kennlinien und -interpolation	              																		*/
+/*	KibaControl		Kennlinien und -interpolation	                                */
 /*					PID-Regler mit Gain-Scheduling und Anti-Windup																				*/
 /*																																																*/
-/*	Frank Kirschbaum (frank.kirschbaum@me.com)               																			*/
+/*	Frank Kirschbaum (frank.kirschbaum@me.com)                                  */
 /*																																																*/
-/*	Kibas Coding Standard and Style Guide:                																				*/
+/*	Kibas Coding Standard and Style Guide:                                    */
 /*  (frei nach http://www.freertos.org/FreeRTOS-Coding-Standard-and-Style-Guide.html)							*/
 /*																																																*/
 /*	Namenskonventionen:																																						*/
 /*	Präfixes für Konstanten, Variablen, Funktionen und Methoden:																	*/
 /*	void/void*								v/pv   void																													*/
-/*	int/int*				   				i/pi		integer																											*/
-/*	uint/uint*				   			ui/pui		unsigned integer																					*/
-/*	int8_t/int8_t*			   		c/pc		char (Byte)																									*/
-/*	uint8_t/uint8_t*		   		uc/puc		unsigned char																							*/
-/*	int16_t/int16_t*		   		s/ps		short																												*/
+/*	int/int*				       i/pi		integer																											*/
+/*	uint/uint*				      ui/pui		unsigned integer																					*/
+/*	int8_t/int8_t*			     c/pc		char (Byte)																									*/
+/*	uint8_t/uint8_t*		     uc/puc		unsigned char																							*/
+/*	int16_t/int16_t*		     s/ps		short																												*/
 /*	uint16_t/uint16_t*				us/pus		unsigned short																						*/
 /*	int32_t/int32_t*					l/pl		long																												*/
 /*	uint32_t/uint32_t*				ul/pul		unsigned long																							*/
@@ -22,7 +22,7 @@
 /*	float/float*							f/pf		float																												*/
 /*	double/double*						d/pd		double																											*/
 /*	BaseType_t/BaseType_t*		x/px		base type, optimal für Registerbreite												*/
-/*	UBaseType_t/UBaseType_t*	ux/pux		unsigned base type, optimal für Registerbreite    				*/
+/*	UBaseType_t/UBaseType_t*	ux/pux		unsigned base type, optimal für Registerbreite        */
 /*	TickType_t/TickType_t*		x/px		16/32 Bit, abhängig von Registerbreite											*/
 /*	size_t/size_t*						x/px																																*/
 /*	TaskHandle_t							pv			Task-handle (Pointer) für die Referenzierung von Tasks			*/
@@ -33,7 +33,7 @@
 /*	Lesbarkeit des Quelltextes:																																		*/
 /*	Space nach ( und vor )																																				*/
 /*	Space nach [ und vor ]																																				*/
-/*	unter jeder Funktionsdeklaration etc. ----...---             																	*/
+/*	unter jeder Funktionsdeklaration etc. ----...---                              */
 /*																																																*/
 /*	'Der Unterschied zwischen Theorie und Praxis ist in der Praxis größer als in der Theorie'			*/
 /*																																																*/
@@ -44,190 +44,190 @@
 /*
  * liefert größstes Element eines Arrays
  */
-int iMaxElement( int* piArray, int8_t ucSize )
-{
-								int iDummy = INT_MIN;     // iDummy = kleinstmögliche Zahl
-								for (int8_t uc=0; uc < ucSize; uc++) // Schleife über alle Stützstellen
-								{
-																if ( piArray[ uc ] > iDummy ) // wenn akluelles Element größer
-																{
-																								iDummy = piArray[ uc ]; // iDummy = neues größtes Element
-																}
-								}
-								return iDummy;
-}
+ int iMaxElement( int* piArray, int8_t ucSize )
+ {
+	 int iDummy = INT_MIN; // iDummy = kleinstmögliche Zahl
+	 for (int8_t uc=0; uc < ucSize; uc++) // Schleife über alle Stützstellen
+	 {
+		 if ( piArray[ uc ] > iDummy ) // wenn akluelles Element größer
+		 {
+			 iDummy = piArray[ uc ]; // iDummy = neues größtes Element
+		 }
+	 }
+	 return iDummy;
+ }
 /*------------------------------------------------------------------------------------------------*/
 /*
  * liefert kleinstes Element eines Arrays
  */
-int iMinElement( int* piArray, int8_t ucSize )
-{
-								int iDummy = INT_MAX;     // iDummy = größtmögliche Zahl
-								for (int8_t uc=0; uc < ucSize; uc++) // Schleife über alle Stützstellen
-								{
-																if ( piArray[ uc ] < iDummy ) // wenn akluelles Element kleiner
-																{
-																								iDummy = piArray[ uc ]; // iDummy = neues kleinstes Element
-																}
-								}
-								return iDummy;
-}
-/*------------------------------------------------------------------------------------------------*/
-/*
- * Kennlinieninterpolation
+ int iMinElement( int* piArray, int8_t ucSize )
+ {
+	 int iDummy = INT_MAX;     // iDummy = größtmögliche Zahl
+	 for (int8_t uc=0; uc < ucSize; uc++) // Schleife über alle Stützstellen
+	 {
+		 if ( piArray[ uc ] < iDummy ) // wenn akluelles Element kleiner
+		 {
+			 iDummy = piArray[ uc ]; // iDummy = neues kleinstes Element
+		 }
+	 }
+	 return iDummy;
+ }
+ /*------------------------------------------------------------------------------------------------*/
+ /*
+  * Kennlinieninterpolation
  */
-int map1D::iOut( int iIn )
-{
-								if ( iIn <= piInVec_[ 0 ] ) return piOutVec_[ 0 ]; // unterhalb des Eingangsbereichs -> konst. Extrap.
-								if ( iIn >= piInVec_[ucVecSize_-1] ) return piOutVec_[ ucVecSize_-1 ]; // oberhalb des Eingangsbereichs -> konst. Extrap.
+ int map1D::iOut( int iIn )
+ {
+	 if ( iIn <= piInVec_[ 0 ] ) return piOutVec_[ 0 ]; // unterhalb des Eingangsbereichs -> konst. Extrap.
+	 if ( iIn >= piInVec_[ucVecSize_-1] ) return piOutVec_[ ucVecSize_-1 ]; // oberhalb des Eingangsbereichs -> konst. Extrap.
 
-								uint8_t ucPos = 1;         // piInVec[0] bereits überprüft
-								while( iIn > piInVec_[ ucPos ] ) ucPos++;  // Intervall suchen
+	 uint8_t ucPos = 1;         // piInVec[0] bereits überprüft
+	 while( iIn > piInVec_[ ucPos ] ) ucPos++;  // Intervall suchen
 
-								if ( iIn == piInVec_[ ucPos ] ) return piOutVec_[ ucPos ]; // Behandlung 'exakter' Punkte
+	 if ( iIn == piInVec_[ ucPos ] ) return piOutVec_[ ucPos ]; // Behandlung 'exakter' Punkte
 
-								// Interpolation zwischen den Stützstellen
-								return ( iIn - piInVec_[ ucPos-1 ] ) * ( piOutVec_[ ucPos ] - piOutVec_[ ucPos-1 ] ) / ( piInVec_[ ucPos ] - piInVec_[ ucPos-1 ] ) + piOutVec_[ ucPos-1 ];
-}
+	 // Interpolation zwischen den Stützstellen
+	 return ( iIn - piInVec_[ ucPos-1 ] ) * ( piOutVec_[ ucPos ] - piOutVec_[ ucPos-1 ] ) / ( piInVec_[ ucPos ] - piInVec_[ ucPos-1 ] ) + piOutVec_[ ucPos-1 ];
+ }
 /*------------------------------------------------------------------------------------------------*/
 /*
  * Setzen bzw. Definieren einer Kennlinie
  */
-uint8_t map1D::ucSetMap( int* piIn, int* piOut, uint8_t ucSize )
-{
-								int iInSize = piIn[ 0 ];    // bisher kleinstes Element = erstes Element
-								for (uint8_t uc=1; uc <= ucSize-1; uc++ ) // Schleife über alle Stützstellen
-								{
-																if ( (piIn[ uc ] < iInSize ) ) // kleiner als bisher kleinstes?
-																{
-																								return 1; // Fehler! keine Monotonie!
-																}
-																iInSize = piIn[ uc ]; // bisher kleinstes Element = aktuelles Element
-								}
-								piInVec_ = piIn;      // Übergabe Zeiger auf Stützstellenvektor
-								piOutVec_ = piOut;      // Übergabe Zeiger auf Vektor mit den Werten an den Stützstellen
-								ucVecSize_ = ucSize;     // Übergabe Anzahl der Stützstellen
+ uint8_t map1D::ucSetMap( int* piIn, int* piOut, uint8_t ucSize )
+ {
+	 int iInSize = piIn[ 0 ];    // bisher kleinstes Element = erstes Element
+	 for (uint8_t uc=1; uc <= ucSize-1; uc++ ) // Schleife über alle Stützstellen
+	 {
+		 if ( (piIn[ uc ] < iInSize ) ) // kleiner als bisher kleinstes?
+		 {
+			 return 1; // Fehler! keine Monotonie!
+		 }
+		 iInSize = piIn[ uc ]; // bisher kleinstes Element = aktuelles Element
+	 }
+	 piInVec_ = piIn;      // Übergabe Zeiger auf Stützstellenvektor
+	 piOutVec_ = piOut;      // Übergabe Zeiger auf Vektor mit den Werten an den Stützstellen
+	 ucVecSize_ = ucSize;     // Übergabe Anzahl der Stützstellen
 
-								iInMin_ = piInVec_[ 0 ];     // kleinste Stützstelle
-								iInMax_ = piInVec_[ ucVecSize_-1 ];  // größte Stützstelle
+	 iInMin_ = piInVec_[ 0 ];     // kleinste Stützstelle
+	 iInMax_ = piInVec_[ ucVecSize_-1 ];  // größte Stützstelle
 
-								iOutMax_ = iMaxElement( piOutVec_, ucVecSize_ ); // maximale Ausgangsgröße
-								iOutMin_ = iMinElement( piOutVec_, ucVecSize_ ); // minimale Ausgangsgröße
+	 iOutMax_ = iMaxElement( piOutVec_, ucVecSize_ ); // maximale Ausgangsgröße
+	 iOutMin_ = iMinElement( piOutVec_, ucVecSize_ ); // minimale Ausgangsgröße
 
-								return 0;         // kein fehler
-}
+	 return 0;         // kein fehler
+ }
 /*------------------------------------------------------------------------------------------------*/
 /*
  * Liefert kleinsten möglichen Ausgangswert einer Kennlinie
  */
 int map1D::iGetMinOut( void )
 {
-								return iOutMin_;
+		return iOutMin_;
 }
 /*------------------------------------------------------------------------------------------------*/
 /*
  * Liefert größten möglichen Ausgangswert einer Kennlinie
  */
-int map1D::iGetMaxOut( void )
-{
-								return iOutMax_;
-}
+ int map1D::iGetMaxOut( void )
+ {
+	 return iOutMax_;
+ }
 /*------------------------------------------------------------------------------------------------*/
 /*
  * Liefert untere Grenze des Wertebereichs der x-Achse
  */
-int map1D::iGetMinSupportingPoint( void )
-{
-								return iInMin_;
-}
+ int map1D::iGetMinSupportingPoint( void )
+ {
+	 return iInMin_;
+ }
 /*------------------------------------------------------------------------------------------------*/
 /*
  * Liefert obere Grenze des Wertebereichs der x-Achse
  */
-int map1D::iGetMaxSupportingPoint( void )
-{
-								return iInMax_;
-}
+ int map1D::iGetMaxSupportingPoint( void )
+ {
+	 return iInMax_;
+ }
 /*------------------------------------------------------------------------------------------------*/
 /*
  * Konstruktor eines Objekts der Klasse 'PIDcontrol'
  */
-PIDcontrol::PIDcontrol( int iKP, int iKI, int iKD, int iOutMin, int iOutMax, map1D Pmap, map1D IMap, map1D DMap)
-{
-								iProportionalGain_ = iKP;
-								iIntegrationalGain_ = iKI;
-								iDifferentialGain_ = iKD;
-								iOutMinLim_ = iOutMin;
-								iOutMaxLim_ = iOutMax;
-								ProportionalMap_ = Pmap;
-								IntegrationalMap_ = IMap;
-								DifferentialMap_ = DMap;
-}
+ PIDcontrol::PIDcontrol( int iKP, int iKI, int iKD, int iOutMin, int iOutMax, map1D Pmap, map1D IMap, map1D DMap)
+ {
+	 iProportionalGain_ = iKP;
+	 iIntegrationalGain_ = iKI;
+	 iDifferentialGain_ = iKD;
+	 iOutMinLim_ = iOutMin;
+	 iOutMaxLim_ = iOutMax;
+	 ProportionalMap_ = Pmap;
+	 IntegrationalMap_ = IMap;
+	 DifferentialMap_ = DMap;
+ }
 /*------------------------------------------------------------------------------------------------*/
 /*
  * Berechnet den Ausgang eines PID-Reglers mit Gain-Scheduling und Anti-Windup
  */
-int PIDcontrol::iOut( int iDesiredValue, int iActualValue )
-{
-								int iError;
-								int iSum;
+ int PIDcontrol::iOut( int iDesiredValue, int iActualValue )
+ {
+	 int iError;
+	 int iSum;
 
-								iError = iDesiredValue - iActualValue;
+	 iError = iDesiredValue - iActualValue;
 
-								if (!ucIsLimited_)
-								{
-																iIntegrator_ = iIntegrator_ + iError * IntegrationalMap_.iOut( iError );
-								}
+	 if (!ucIsLimited_)
+	 {
+		 iIntegrator_ = iIntegrator_ + iError * IntegrationalMap_.iOut( iError );
+	 }
 
-								iSum = iProportionalGain_ * iError + iIntegrator_;
+	 iSum = iProportionalGain_ * iError + iIntegrator_;
 
-								if ( iSum <= iOutMinLim_ )
-								{
-																ucIsLimited_ = 1;
-																return iOutMinLim_;
-								}
-								else if (iSum >= iOutMaxLim_)
-								{
-																ucIsLimited_ = 1;
-																return iOutMaxLim_;
-								}
-								else
-								{
-																ucIsLimited_ = 0;
-																return iSum;
-								}
-}
+	 if ( iSum <= iOutMinLim_ )
+	 {
+		 ucIsLimited_ = 1;
+		 return iOutMinLim_;
+	 }
+	 else if (iSum >= iOutMaxLim_)
+	 {
+		 ucIsLimited_ = 1;
+		 return iOutMaxLim_;
+	 }
+	 else
+	 {
+		 ucIsLimited_ = 0;
+		 return iSum;
+	 }
+ }
 /*------------------------------------------------------------------------------------------------*/
 /*
  * Setzen der Proportionalverstärkung
  */
-void PIDcontrol::vSetProportionalGain(int iKP)
-{
-								iProportionalGain_ = iKP;
-}
+ void PIDcontrol::vSetProportionalGain(int iKP)
+ {
+	 iProportionalGain_ = iKP;
+ }
 /*------------------------------------------------------------------------------------------------*/
 /*
  * Setzen der Integralverstärkung
  */
-void PIDcontrol::vSetIntegrationalGain(int iKI)
-{
-								iIntegrationalGain_ = iKI;
-}
+ void PIDcontrol::vSetIntegrationalGain(int iKI)
+ {
+	 iIntegrationalGain_ = iKI;
+ }
 /*------------------------------------------------------------------------------------------------*/
 /*
  * Setzt die Differentialverstärkung
  */
-void PIDcontrol::vSetDifferentialGain(int iKD)
-{
-								iDifferentialGain_ = iKD;
-}
+ void PIDcontrol::vSetDifferentialGain(int iKD)
+ {
+	 iDifferentialGain_ = iKD;
+ }
 /*------------------------------------------------------------------------------------------------*/
 /*
  * Setzt die untere Reglerausgangsbeschränkung
  */
 void PIDcontrol::vSetOutMinLimit( int iOutMin )
 {
-								iOutMinLim_ = iOutMin;
+	iOutMinLim_ = iOutMin;
 }
 /*------------------------------------------------------------------------------------------------*/
 /*
@@ -235,7 +235,7 @@ void PIDcontrol::vSetOutMinLimit( int iOutMin )
  */
 void PIDcontrol::vSetOutMaxLimit( int iOutMax )
 {
-								iOutMaxLim_ = iOutMax;
+	iOutMaxLim_ = iOutMax;
 }
 /*------------------------------------------------------------------------------------------------*/
 /*
@@ -243,7 +243,7 @@ void PIDcontrol::vSetOutMaxLimit( int iOutMax )
  */
 void PIDcontrol::vSetProportionalGainScheduling( map1D Pmap )
 {
-								ProportionalMap_ = Pmap;
+	ProportionalMap_ = Pmap;
 }
 /*------------------------------------------------------------------------------------------------*/
 /*
@@ -251,7 +251,7 @@ void PIDcontrol::vSetProportionalGainScheduling( map1D Pmap )
  */
 void PIDcontrol::vSetIntegrationalGainScheduling( map1D Imap )
 {
-								IntegrationalMap_ = Imap;
+	IntegrationalMap_ = Imap;
 }
 /*------------------------------------------------------------------------------------------------*/
 /*
@@ -259,7 +259,7 @@ void PIDcontrol::vSetIntegrationalGainScheduling( map1D Imap )
  */
 void PIDcontrol::vSetDifferentialGainScheduling( map1D Dmap )
 {
-								DifferentialMap_ = Dmap;
+	DifferentialMap_ = Dmap;
 }
 /*------------------------------------------------------------------------------------------------*/
 /*
@@ -267,8 +267,8 @@ void PIDcontrol::vSetDifferentialGainScheduling( map1D Dmap )
  */
 void PIDcontrol::vResetIntegrator( void )
 {
-								iIntegrator_ = 0;
-								ucIsLimited_ = 0;
+	iIntegrator_ = 0;
+	ucIsLimited_ = 0;
 }
 /*------------------------------------------------------------------------------------------------*/
 /*
@@ -276,6 +276,6 @@ void PIDcontrol::vResetIntegrator( void )
  */
 uint8_t PIDcontrol::ucGetAntiWindupState( void )
 {
-								return ucIsLimited_;
+	return ucIsLimited_;
 }
 /*------------------------------------------------------------------------------------------------*/
